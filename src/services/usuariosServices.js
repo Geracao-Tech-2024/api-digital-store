3d272140ec237ee91b2e7c34c852ae47c9cabac9
MARCOS LEVI ESTEVAO DA SILVA <MARCOS0866@DIGITALCOLLEGE.LOCAL>
2024-08-09 16:59:17 -0300

    modis

--- src/services/usuariosServices.js
+++ src/services/usuariosServices.js
@@ -1,28 +1,32 @@
-const user = require('../models/User')
+const User = require("../models/User");
+const express = require("express");
+
+const router = express.Router();
 
 class UsuarioServices {
-    getUsuarios(){
-        return 'todos os usuarios'
-    }
-    
-    getUsuario(req){
-      const {id} = req.params.id;
-      const project = user.findById(id);
-      if (user === null){
-        return "NOT FOUND"
-      
-      }else{
-        // console.log();
-      }
-    }
-    postUsuario(){
-        return 'usuario criado'
-    }
-    putUsuario(){
-        return 'usuario atualizado'
-    }
-    deleteUsuario(){
-        return 'usuario deletado'
-    }
+  getUsuarios() {
+    return "todos os usuarios";
+  }
+
+  async getUsuario(req, res) {
+    // const user = await User.findByPk(req.params.id);
+    // console.log(user)
+    // if (!user || user == {}) {
+    //     return res.status(404).json({ message: 'Usuário não encontrado' });
+    // }
+    //  res.status(200).json(user);
+  }
+
+
+  postUsuario() {
+    return "usuario criado";
+  }
+  putUsuario() {
+    return "usuario atualizado";
+  }
+  deleteUsuario() {
+    return "usuario deletado";
+  }
 }
-module.exports =  new UsuarioServices();
+
+module.exports = new UsuarioServices();
