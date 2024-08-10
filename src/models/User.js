24a4ba966b9cf900bf4e1784756e935166ad94c5
Lucas Maciel <maciellucas487@gmail.com>
2024-08-10 08:50:19 -0300

    user model update

--- src/models/User.js
+++ src/models/User.js
@@ -28,16 +28,6 @@ const User = sequelize.define('User', {
 }, {
     tableName: 'users',
     timestamps: true,
-    hooks: {
-        beforeCreate: async (user) => {
-            user.password = await bcrypt.hash(user.password, 10);
-        },
-        beforeUpdate: async (user) => {
-            if (user.changed('password')) {
-                user.password = await bcrypt.hash(user.password, 10);
-            }
-        },
-    },
 });
 
 module.exports = User;
