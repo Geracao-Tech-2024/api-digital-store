7cf9a33437d0d2a083f7de14d14346d9806e1b5b
LUCAS MACIEL DOS SANTOS <LUCAS0721@DIGITALCOLLEGE.LOCAL>
2024-08-13 16:24:52 -0300

    rota de delete iniciando, deixando salvo antes de ir pra casa

--- src/services/categoryServices.js
+++ src/services/categoryServices.js
@@ -1,10 +1,12 @@
-const user = require('../models/Category')
+const Category = require('../models/Category')
 
 class CategoriesServices{
     async getCategorys(){
         return 'todos os usuarios';
     }
-
+    async deleteCategory(){
+        return {messege: 'usuario deletado', status: 200}
+    }
 }
 
 module.exports =  new CategoriesServices();
