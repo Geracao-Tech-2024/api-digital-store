97a3738bc3f188a65d100334b5f63564269c95ad
Lucas Maciel <maciellucas487@gmail.com>
2024-08-17 08:13:40 -0300

    jwt em POST category adicionado

--- src/routes/routeCategorys.js
+++ src/routes/routeCategorys.js
@@ -1,13 +1,13 @@
 // faça a chamada da função dos controllers (é onde fica a regra de negocios)
 const Controller = require('./../controller/categoryController');
-
+const jwt_alth = require('./../middleware/jwt_alth');
 const express = require('express');
 const category = express();
 
 category.get('/:id', Controller.getCategoryById)
 category.get('/', Controller.getAllCategorys);
-category.put('/:id',Controller.updateCategory);
+category.put('/:id', Controller.updateCategory);
 category.delete('/:id', Controller.deleteCategory);
-category.post('/', Controller.postCategory);
+category.post('/', jwt_alth.verifyJWT,Controller.postCategory);
 
 module.exports = category;
