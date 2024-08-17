d6b3f7966aa026026d04209f06e5c3993314ffa1
Lucas Maciel <maciellucas487@gmail.com>
2024-08-17 16:06:47 -0300

    verificação JWT adicionado as rotas de Produtos

--- src/routes/routeProducts.js
+++ src/routes/routeProducts.js
@@ -1,12 +1,13 @@
 // faça a chamada da função dos controllers (é onde fica a regra de negocios)
 const Controller = require("../controller/productController.js");
-
+const jwt_alth = require('./../middleware/jwt_alth');
 const express = require("express");
 const product = express();
+
 product.get('/search', Controller.getAllProducts);
 product.get("/:id", Controller.getProductById);
-product.post("/", Controller.postProducts);
-product.put("/:id", Controller.updateProduct);
-product.delete("/:id", Controller.deleteProduct);
+product.post("/", jwt_alth.verifyJWT, Controller.postProducts);
+product.put("/:id", jwt_alth.verifyJWT, Controller.updateProduct);
+product.delete("/:id", jwt_alth.verifyJWT, Controller.deleteProduct);
 
 module.exports = product;
