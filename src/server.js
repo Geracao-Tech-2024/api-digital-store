42d74e4202d0f2515d989578f7148fa60004775c
Lucas Maciel <maciellucas487@gmail.com>
2024-08-18 23:06:25 -0300

    cofig para deploy

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const app = require('./app');
-
+const PORT = process.env.PORT || 3500;
 // impedindo a inicialização do servidor caso o banco de dados nao esteja conectado
 app.on('db_ok', ()=>{
     console.log('iniciando o servidor...');
-    app.listen(3500, 'localhost', ()=>{
+    app.listen(PORT, 'localhost', ()=>{
         console.log('rodando na porta: http://localhost:3500');
     });
 });
