038afbce489b69ffe4fe6ac7bed6571341de40bf
Lucas Maciel <maciellucas487@gmail.com>
2024-08-18 23:49:55 -0300

    cofig localhost

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const porta = process.env.PORT || 3500;
 // impedindo a inicialização do servidor caso o banco de dados nao esteja conectado
 app.on('db_ok', ()=>{
     console.log('iniciando o servidor...');
-    app.listen(porta, 'localhost', ()=>{
-        console.log('rodando na porta: http://localhost:', porta);
+    app.listen(porta, '0.0.0.0', ()=>{
+        console.log('rodando na porta: http://0.0.0.0:', porta);
     });
 });
